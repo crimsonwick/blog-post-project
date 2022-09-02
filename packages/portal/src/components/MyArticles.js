040f3548d9ffb42c4315f752235c260329a846ed
Nauman Cheema <nauman.ahmed@kwanso.com>
2022-09-02 18:14:09 +0500

    modified project directory  structure

---
+++ packages/portal/src/components/MyArticles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import NavbarLoggedIn from './NavbarLoggedIn';
+
+function MyArticles() {
+  let isLoggedIn = true;
+  return (
+    <div>
+      {!isLoggedIn && <Navbar></Navbar>}
+      {isLoggedIn && <NavbarLoggedIn></NavbarLoggedIn>}
+      <div>MyArticles</div>
+    </div>
+  );
+}
+
+export default MyArticles;
