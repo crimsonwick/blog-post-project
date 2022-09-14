b80d7f1ed636e145e053ad352cef29edfd195c05
kwanso-saif <saif.ulhaq@kwanso.com>
2022-09-14 16:41:37 +0500

    divider added

--- packages/portal/src/pages/MyArticles.js
+++ packages/portal/src/pages/MyArticles.js
@@ -1,15 +1,21 @@
-import { Container } from '@mui/system';
-import React from 'react';
-import NavBar from '../components/NavBar';
-import Article from '../components/Article';
-import { Box } from '@mui/system';
-import Footer from '../components/Footer';
+import { Container } from "@mui/system";
+import React from "react";
+import NavBar from "../components/NavBar";
+import Article from "../components/Article";
+import { Box } from "@mui/system";
+import Footer from "../components/Footer";
+import { Divider } from "@mui/material";
+import NavBarX from "../components/NavBarX";
 
 const MyArticles = () => {
   return (
     <>
-      <NavBar />
-      <Container maxWidth="lg" sx={{ position: 'relative' }}>
+      <NavBarX login = {true}></NavBarX>
+
+      <Container maxWidth="lg" sx={{ position: "relative" }}>
+        <h1 style={{fontFamily:"Poppins", marginTop: "65px"}}>Recent Posts</h1>
+        <Divider></Divider>
+
         <Box mt={5}>
           <Article />
           <Article />
