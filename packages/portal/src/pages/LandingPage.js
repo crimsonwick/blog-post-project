83c61d7f8ece49ed9b04faa08834f4e5ad562a9d
Nauman Cheema <nauman.ahmed@kwanso.com>
2022-09-28 16:37:44 +0500

    added pkgs

--- packages/portal/src/pages/LandingPage.js
+++ packages/portal/src/pages/LandingPage.js
@@ -10,35 +10,38 @@ import { AppContext } from '../App';
 const Home = () => {
   const [data, setData] = useState([]);
 
-  const { loggedIn, searchData } = useContext(AppContext)
+  const { loggedIn, searchData } = useContext(AppContext);
   const allPosts = async () => {
     const details = await allPostsComing();
-    setData(details.data)
-  }
+    setData(details.data);
+  };
 
   useEffect(() => {
-
     allPosts();
   }, []);
   return (
     <>
-      {(loggedIn) ? (<NavBar style={{ position: 'fixed' }} login={true} />) : (<NavBar style={{ position: 'fixed' }} />)}
+      {loggedIn ? (
+        <NavBar style={{ position: 'fixed' }} login={true} />
+      ) : (
+        <NavBar style={{ position: 'fixed' }} />
+      )}
 
       <Container sx={{ marginY: 20 }}>
-        <h1 style={{ fontFamily: "Poppins", marginTop: "65px" }}>Recent Posts</h1>
-        <Divider></Divider>
-
+        <h1 style={{ fontFamily: 'Poppins', marginTop: '65px' }}>
+          Recent Posts
+        </h1>
+        <Divider />
         <Box mt={5}>
-          {(data && searchData.length === 0) ? ((data.map((object) => {
-            return (
-              <Article key={object.id} object={object} />
-            )
-          }))
-          ) : (searchData.map((object) => {
-            return (
-              <Article key={object._source.id} object={object._source} />
-            )
-          }))}
+          {data && searchData.length === 0
+            ? data.map((object) => {
+                return <Article key={object.id} object={object} />;
+              })
+            : searchData.map((object) => {
+                return (
+                  <Article key={object._source.id} object={object._source} />
+                );
+              })}
         </Box>
         <Footer />
       </Container>
