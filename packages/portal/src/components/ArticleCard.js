3ae6becaf4dae52466a2526687b839e8fa03713b
kwanso-saif <saif.ulhaq@kwanso.com>
2022-10-03 16:12:21 +0500

    ayo

--- packages/portal/src/components/ArticleCard.js
+++ packages/portal/src/components/ArticleCard.js
@@ -1,31 +1,55 @@
-import React from 'react';
-import { Box } from '@mui/system';
-import '../styles/Article/Article.css';
-import { Card, List } from '@mui/material';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
-import flexContainer from '../styles/Article/List';
-import { Avatar } from '@mui/material';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
-import Chip from '@mui/material/Chip';
-import { parseDate, parseName } from '../services/LoginApi';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Box } from "@mui/system";
+import "../styles/Article/Article.css";
+import { Card, List } from "@mui/material";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import flexContainer from "../styles/Article/List";
+import { Avatar } from "@mui/material";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
+import Chip from "@mui/material/Chip";
+import { parseDate, parseName } from "../services/LoginApi";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 const ArticleCard = (props) => {
+  const { dp, userData } = useContext(AppContext);
 
   return (
     <Card
-      elevation={10}
-      sx={{ display: 'flex', allignItems: 'centre', marginTop: '20px' }}
+      style={{
+        marginTop: "30px",
+        hieght: "auto",
+        width: "auto",
+        opacity: "100%",
+        border: "none",
+        boxShadow: "none",
+      }}
+      sx={{ display: "flex", allignItems: "centre", marginTop: "20px" }}
     >
-      <img src={require(`../images/${props.object.image}`)} alt="user_image" className="articleImg"/>
+      <img
+        src={require(`../images/${props.object.image}`)}
+        alt="user_image"
+        className="articleImg"
+        style={{
+          borderRadius: "20px",
+          width: "300px",
+          height: "250px",
+          objectFit: "fill",
+          marginRight: "50px",
+        }}
+      />
       <Box mt={1}>
         <Chip label="Travel" />
-        <Link to="/article-detail" state={props} style={{textDecoration: "none" , color: "rgba(0,0,0,0.87)"}}>
+        <Link
+          to="/article-detail"
+          state={props}
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <Typography variant="h4" component="h3">
             {props.object.title}
           </Typography>
@@ -34,11 +58,18 @@ const ArticleCard = (props) => {
           <ListItem className="user">
             <ListItemIcon>
               <Avatar
-                src={require(`../images/${props.object.Posted_By.avatar}`)}
-                alt="spongebob"
+                alt="user display picture"
+                src={
+                  dp
+                    ? require(`../images/${dp}`)
+                    : userData.avatar
+                    ? require(`../images/${userData.avatar}`)
+                    : ""
+                }
+                sx={{ width: 32, height: 32 }}
               />
             </ListItemIcon>
-            <ListItemText primary={parseName(props.object.Posted_By.email)}/>
+            <ListItemText primary={`${props.object.userId}`} />
           </ListItem>
           <ListItem className="date">
             <ListItemIcon>
@@ -53,7 +84,7 @@ const ArticleCard = (props) => {
             <ListItemText primary={`${props.object.timetoRead} Min. To Read`} />
           </ListItem>
         </List>
-        <Typography variant="h6">
+        <Typography variant="h6" style={{ objectFit: "fill" }}>
           {props.object.body}
         </Typography>
       </Box>
--- packages/portal/src/components/NavBar.js
+++ packages/portal/src/components/NavBar.js
@@ -17,11 +17,13 @@ import Logout from '@mui/icons-material/Logout';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
-import { useContext,useState } from 'react';
+import { logout } from '../services/LoginApi.js';
+import { useContext, useState } from 'react';
 import { AppContext } from '../App.js';
-import { searchAPI,logout } from '../services/LoginApi.js';
+import { searchAPI } from '../services/LoginApi.js';
 import { Typography } from '@mui/material';
-
+import flexContainer from '../styles/Article/List';
+import { Box } from '@mui/material';
 const Navbar = ({ login }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const { setLoggedIn, setSearchData, refreshToken, userData, dp } =
@@ -32,14 +34,6 @@ const Navbar = ({ login }) => {
   };
   const handleClose = () => {
     setAnchorEl(null);
-  };
-
-  const handleKeyDown = async (event) => {
-    const response = await searchAPI(event.target.value);
-    setSearchData(response.data);
-  };
-  const handleChange = (event) => {
-    [event.target.name] = [event.target.value]
   };
   const handleLogout = async () => {
     try {
@@ -52,14 +46,27 @@ const Navbar = ({ login }) => {
     }
   };
   return (
-    <AppBar position="fixed" style={{ background: '#FFFFFF' }}>
+    <AppBar position="static" style={{ background: '#FFFFFF' }}>
       <Toolbar>
-        <Link
-          to="/"
-          style={{ padding: 10, textDecoration: 'none', color: 'black' }}
-        >
-          Home
-        </Link>
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            sx={{
+              // flex: 1,
+              marginLeft: '5px',
+              fontWeight: '600',
+              textTransform: 'capitalize',
+              // flexGrow: 0,
+              // flexShrink: 0,
+              // flexBasis: '70px',
+              color: '#111111',
+              textDecoration: 'none',
+            }}
+          >
+            Home
+          </Typography>
           {login && (
             <Typography
               component={Link}
@@ -71,157 +78,155 @@ const Navbar = ({ login }) => {
                 textTransform: 'capitalize',
                 textDecoration: 'none',
                 paddingLeft: '20px',
+                fontWeight: '600',
                 color: '#111111',
               }}
             >
               My Articles
             </Typography>
           )}
-          <Search sx={{ color: '#111111' }}>
-            <SearchIconWrapper>
-              <SearchIcon sx={{ color: '#111111' }} />
-            </SearchIconWrapper>
-
-            <StyledInputBase
-              sx={{ color: '#111111' }}
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              onChange={handleChange}
-              onKeyDown={handleKeyDown}
-            />
-          </Search>
-          {login && (
+        </Box>
+        <Search sx={{ color: '#111111' }}>
+          <SearchIconWrapper>
+            <SearchIcon sx={{ color: '#111111' }} />
+          </SearchIconWrapper>
+          <StyledInputBase
+            sx={{ color: '#111111' }}
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+        {login && (
+          <Button
+            component={Link}
+            to="/create-article"
+            variant="contained"
+            color="secondary"
+            sx={{
+              fontWeight: '600',
+              textTransform: 'capitalize',
+              marginLeft: '15px',
+            }}
+          >
+            Create Article
+          </Button>
+        )}
+        {!login && (
+          <div>
             <Button
               component={Link}
-              to="/create-article"
+              to="/login"
+              variant="contained"
+              color="primary"
+              sx={{ marginRight: '10px' }}
+            >
+              Login
+            </Button>
+            <Button
+              component={Link}
+              to="/signup"
               variant="contained"
               color="secondary"
-              sx={{
-                fontWeight: '600',
-                textTransform: 'capitalize',
-                marginLeft: '15px',
-              }}
             >
-              Create Article
+              Sign Up
             </Button>
-          )}
-          {!login && (
-            <div>
-              <Button
-                component={Link}
-                to="/login"
-                variant="contained"
-                color="primary"
-                sx={{ marginRight: '10px' }}
+          </div>
+        )}
+        {login && (
+          <div>
+            <Tooltip title="Account settings">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
               >
-                Login
-              </Button>
-              <Button
-                component={Link}
-                to="/signup"
-                variant="contained"
-                color="secondary"
+                <Avatar
+                  alt="user display picture"
+                  src={
+                    dp
+                      ? require(`../images/${dp}`)
+                      : userData.avatar
+                      ? require(`../images/${userData.avatar}`)
+                      : ''
+                  }
+                  sx={{ width: 32, height: 32 }}
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  '&:before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <Link
+                to="/account-details"
+                style={{ textDecoration: 'none', color: 'black' }}
               >
-                Sign Up
-              </Button>
-            </div>
-          )}
-          {login && (
-            <div>
-              <Tooltip title="Account settings">
-                <IconButton
-                  onClick={handleClick}
-                  size="small"
-                  sx={{ ml: 2 }}
-                  aria-controls={open ? 'account-menu' : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? 'true' : undefined}
-                >
+                <MenuItem>
                   <Avatar
                     alt="user display picture"
                     src={
-                      userData.avatar
-                        ? require(`../images/${userData.avatar}`)
-                        : dp
+                      dp
                         ? require(`../images/${dp}`)
+                        : userData.avatar
+                        ? require(`../images/${userData.avatar}`)
                         : ''
                     }
-                    sx={{ width: 32, height: 32 }}
                   />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                anchorEl={anchorEl}
-                id="account-menu"
-                open={open}
-                onClose={handleClose}
-                onClick={handleClose}
-                PaperProps={{
-                  elevation: 0,
-                  sx: {
-                    overflow: 'visible',
-                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                    mt: 1.5,
-                    '& .MuiAvatar-root': {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
-                    },
-                    '&:before': {
-                      content: '""',
-                      display: 'block',
-                      position: 'absolute',
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: 'background.paper',
-                      transform: 'translateY(-50%) rotate(45deg)',
-                      zIndex: 0,
-                    },
-                  },
-                }}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                  My account
+                </MenuItem>
+              </Link>
+              <Divider />
+              <Link
+                to="/"
+                style={{ textDecoration: 'none', color: 'black' }}
+                onClick={handleLogout}
               >
-                <Link
-                  to="/account-details"
-                  style={{ textDecoration: 'none', color: 'black' }}
-                >
-                  <MenuItem>
-                    <Avatar
-                      alt="user display picture"
-                      src={
-                        userData.avatar
-                          ? require(`../images/${userData.avatar}`)
-                          : dp
-                          ? require(`../images/${dp}`)
-                          : ''
-                      }
-                    />
-                    My account
-                  </MenuItem>
-                </Link>
-                <Divider />
-                <Link
-                  to="/"
-                  style={{ textDecoration: 'none', color: 'black' }}
-                  onClick={handleLogout}
-                >
-                  <MenuItem>
-                    <ListItemIcon>
-                      <Logout fontSize="small" />
-                    </ListItemIcon>
-                    Logout
-                  </MenuItem>
-                </Link>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
+                <MenuItem>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  Logout
+                </MenuItem>
+              </Link>
+            </Menu>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
-
 export default Navbar;
