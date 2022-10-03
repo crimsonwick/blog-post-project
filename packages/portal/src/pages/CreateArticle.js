e8e04a7e3faaa643dbc202a39ed2a9f12660b58d
kwanso-talha <talha.shakil@kwanso.com>
2022-10-03 17:48:13 +0500

    feature/design-improvement

--- packages/portal/src/pages/CreateArticle.js
+++ packages/portal/src/pages/CreateArticle.js
@@ -205,7 +205,7 @@ function CreateArticle() {
             {isDragActive ? (
               <p>Drop the files here ...</p>
             ) : (
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <label className={styles.poppins}>Drag Files Here</label>
             )}
           </div>
           <br />
