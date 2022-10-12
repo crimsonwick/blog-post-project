16cb13768a0972345813db1561704f80d99f8825
kwanso-farhanAmin <farhan.amin@kwanso.com>
2022-10-12 18:45:10 +0500

    feature-Re-routing-apis-fixed

--- packages/api/src/middleware/Authentication.js
+++ packages/api/src/middleware/Authentication.js
@@ -3,13 +3,13 @@ import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
-export const Authentication = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(401);
-    req.user = user;
-    next();
-  });
-};
+  export const Authentication = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(401);
+      req.user = user;
+      next();
+    });
+  };
--- packages/api/src/middleware/Errors.js
+++ packages/api/src/middleware/Errors.js
@@ -1,27 +1,26 @@
-export const ErrorHandling = (res, status = 500) => {
-  // console.log(">>>>>>>>>>>>>>>>>>>>>res", res)
-  const error = { status, message: "" }
+  export const ErrorHandling = (res, status = 500) => {
+    const error = { status, message: "" }
 
-  switch (status) {
-    case 400:
-      error.status = 400;
-      error.message = "Bad request";
-      break;
-    case 401:
-      error.status = 401;
-      error.message = "Invalid Authentication Credentials";
-      break;
-    case 402:
-      error.status = 402;
-      error.message = "Payment Required";
-      break;
-    case 403:
-      error.status = 403;
-      error.message = "Forbidden response";
-    default: 404;
-      error.status = 404
-      error.message = "User Not Found";
+    switch (status) {
+      case 400:
+        error.status = 400;
+        error.message = "Bad request";
+        break;
+      case 401:
+        error.status = 401;
+        error.message = "Invalid Authentication Credentials";
+        break;
+      case 402:
+        error.status = 402;
+        error.message = "Payment Required";
+        break;
+      case 403:
+        error.status = 403;
+        error.message = "Forbidden response";
+      default: 404;
+        error.status = 404
+        error.message = "User Not Found";
+    }
+
+    return res.json(error);
   }
-
-  return res.json(error);
-}
