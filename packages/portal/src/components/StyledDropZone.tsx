8be6d380a02cc139cd8e10654c1b770c64d7583c
kwanso-saif <saif.ulhaq@kwanso.com>
2022-10-21 12:49:15 +0500

    updated code

--- packages/portal/src/components/StyledDropZone.tsx
+++ packages/portal/src/components/StyledDropZone.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback, useContext } from 'react';
-import { useDropzone } from 'react-dropzone';
-import styled from 'styled-components';
-import { AppContext } from '../App';
-import { AppContextInterface, UserInterface } from '../interface/App';
+import React, { useCallback, useContext, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import styled from "styled-components";
+import { AppContext } from "../App";
+import { AppContextInterface, UserInterface } from "../interface/App";
+import "../styles/
 
 interface ColorInterface {
   isDragAccept: boolean;
@@ -12,15 +13,15 @@ interface ColorInterface {
 
 const getColor = (props: ColorInterface) => {
   if (props.isDragAccept) {
-    return '#00e676';
+    return "#00e676";
   }
   if (props.isDragReject) {
-    return '#ff1744';
+    return "#ff1744";
   }
   if (props.isFocused) {
-    return '#2196f3';
+    return "#2196f3";
   }
-  return '#eeeeee';
+  return "#eeeeee";
 };
 
 const Container = styled.div`
@@ -40,31 +41,86 @@ const Container = styled.div`
   transition: border 0.24s ease-in-out;
 `;
 
+interface MyFile {
+  preview: string;
+  name: string;
+}
+
 export const StyledDropZone = () => {
+  const [myFile, setMyFile] = useState<MyFile[]>([]);
+
   const context: AppContextInterface<UserInterface> | null =
     useContext(AppContext);
+
   const onDrop = useCallback(
     (acceptedFile: File[]) => {
       const file = acceptedFile[0];
+      setMyFile(
+        acceptedFile.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
       context?.setPostImage(file);
     },
-    [context]
+    [context, myFile]
   );
+
+  const removeFile = (file: MyFile) => () => {
+    setMyFile([]);
+  };
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     maxFiles: 1,
     multiple: false,
     onDrop,
+    accept: {
+      "image/jpeg": [],
+      "image/jpg": [],
+      "image/png": [],
+    },
   });
 
+  const removeFileList = myFile.map((file_) => (
+    // <li key={file_.path}>
+    //   {file_.path} - {file_.size} bytes{" "}
+    <button onClick={removeFile(file_)}>Remove File</button>
+    ///* </li> */}
+  ));
+
+  const thumbs = myFile.map((file) => (
+    <div key={file.name} className='thumb'>
+      <div className="thumbInner">
+        <img
+          src={file.preview}
+          className='img'
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
+        />
+      </div>
+    </div>
+  ));
+
   return (
-    <Container {...getRootProps()}>
-      <input {...getInputProps()} type='file' name='file' accept='image/*' />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
-    </Container>
+    <div>
+      <Container {...getRootProps()}>
+        <input {...getInputProps()} type="file" name="file" accept="image/*" />
+        {isDragActive ? (
+          <p>Drop the file here ...</p>
+        ) : (
+          <p>Drag 'n' drop file here, or click to select file</p>
+        )}
+      </Container>
+
+      <div className='thumbsContainer'>{thumbs}</div>
+      {removeFileList}
+    </div>
   );
 };
+
+//resources:
+//https://react-dropzone.js.org/#!/Previews
+//https://stackoverflow.com/questions/56025690/how-to-remove-file-from-react-dropzone#:~:text=You%27ve%20probably%20already%20got,%2F%2F%20update%20the%20state%20%7D%3B
+//https://react-dropzone.org/#!/Accepting%20specific%20file%20types
